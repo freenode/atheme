6b70b66be7cb441ce50b3422d70691c3e95b403c
Ed Kellett <e@kellett.im>
2017-11-06 21:51:00 +0000

    Don't reveal oper status in /whois

--- libathemecore/ptasks.c
+++ libathemecore/ptasks.c
@@ -334,8 +334,8 @@ void handle_whois(user_t *u, const char *target)
 		numeric_sts(me.me, 312, u, "%s %s :%s", t->nick, t->server->name, t->server->desc);
 		if (t->flags & UF_AWAY)
 			numeric_sts(me.me, 301, u, "%s :Gone", t->nick);
-		if (is_ircop(t))
-			numeric_sts(me.me, 313, u, "%s :%s", t->nick, is_service(t) ? "is a Network Service" : "is an IRC Operator");
+		if (is_service(t))
+			numeric_sts(me.me, 313, u, "%s :%s", t->nick, "is a Network Service");
 		if (t->myuser && !(t->myuser->flags & MU_WAITAUTH))
 			numeric_sts(me.me, 330, u, "%s %s :is logged in as", t->nick, entity(t->myuser)->name);
 	}
